61d34f4745a5fe26d7c9783dd7b82f76c4fe3c91
jewells <jewellsjoshi437@gmail.com>
2020-09-09 14:31:35 +0530

    Updates

---
+++ precache-manifest.85b6e03a07c5287a888e96f5a71fc2f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f48236f296ad85477d62de28f4812c",
+    "url": "/Airbnb-Clone/index.html"
+  },
+  {
+    "revision": "a4a8ef4c71c920374265",
+    "url": "/Airbnb-Clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "60490792bbd2a1b8bbd0",
+    "url": "/Airbnb-Clone/static/css/main.3b7c784c.chunk.css"
+  },
+  {
+    "revision": "a4a8ef4c71c920374265",
+    "url": "/Airbnb-Clone/static/js/2.28de6caa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Airbnb-Clone/static/js/2.28de6caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60490792bbd2a1b8bbd0",
+    "url": "/Airbnb-Clone/static/js/main.4b46aca3.chunk.js"
+  },
+  {
+    "revision": "36aec36883ee2ff3dcb7",
+    "url": "/Airbnb-Clone/static/js/runtime-main.a1cfb376.js"
+  }
+]);
